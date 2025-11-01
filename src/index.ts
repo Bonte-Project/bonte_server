e196bd11f46baf4e8bc5f1a64bbc5682f6aded27
Andrii Salohub <andrejsalogub3@gmail.com>
2025-11-01 20:06:46 +0200

    feat: add Swagger API documentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './swagger.config';
+import cookieParser from 'cookie-parser';
+import usersRoutes from './modules/users/users.routes';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/users', usersRoutes);
 
 const PORT = parseInt(process.env.PORT || '10000', 10);
 const HOST = '0.0.0.0';
